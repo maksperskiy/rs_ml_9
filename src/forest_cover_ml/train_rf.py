825b644d9b0ba69bdc1d8db90701db17a1a446f8
Maksperskiy <maksukay@mail.ru>
2022-05-03 17:15:12 +0300

    add model preds

--- src/forest_cover_ml/train_rf.py
+++ src/forest_cover_ml/train_rf.py
@@ -1,13 +1,15 @@
+from io import BytesIO
 from pathlib import Path
-from joblib import dump
+from joblib import dump, load
 import numpy as np
 
 import click
 import mlflow
 import mlflow.sklearn
+import pandas as pd
 from sklearn.model_selection import cross_validate
 
-from .data import get_X_y
+from .data import get_X_y, get_dataset
 from .rf_pipeline import create_pipeline, CRITERION
 
 
@@ -101,6 +103,45 @@ def train(
             mlflow.log_metric(name, np.average(scores[name]))
             click.echo(f"{name}: {np.average(scores[name])}.")
 
+        features_train, features_val, target_train, target_val = get_dataset(
+            train_dataset_path,
+            random_state,
+            1/folds,
+        )
+        pipeline.fit(features_train, target_train)
         dump(pipeline, save_model_path)
 
         click.echo(f"Model is saved to {save_model_path}.")
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--test-dataset-path",
+    default="data/test.csv",
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
+)
+@click.option(
+    "-m",
+    "--model-path",
+    default="models/model.joblib",
+    type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
+)
+def test_on_last_model(
+    test_dataset_path: Path,
+    model_path: Path
+) -> None:
+    X = pd.read_csv(test_dataset_path)
+    
+    model = load(model_path)
+    
+    y_pred = model.predict(X)
+    
+    predictions_test = pd.DataFrame(y_pred)
+    predictions_test.columns = ['Cover_Type']
+
+    result = pd.concat([X["Id"], predictions_test], axis=1)
+
+    result.to_csv('data/results.csv', index=False)
